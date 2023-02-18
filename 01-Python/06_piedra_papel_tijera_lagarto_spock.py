930160c8baa2c3a11686b98e2d81b96282d63e4c
kkrotDev <moyo.gsxr@gmail.com>
2023-02-18 16:21:13 +0100

    Añadida la solución en JS al reto Reto #6, ademas de añadir control de errores a la solución de python

--- 01-Python/06_piedra_papel_tijera_lagarto_spock.py
+++ 01-Python/06_piedra_papel_tijera_lagarto_spock.py
@@ -1,9 +1,9 @@
-# * Crea un programa que calcule quien gana más partidas al piedra,
-# * papel, tijera, lagarto, spock.
+# * Crea un programa que calcule quien gana más partidas al Rock,
+# * Paper, Scissors, Lizard, Spock.
 # * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
 # * - La función recibe un listado que contiene pares, representando cada jugada.
-# * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
-# *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+# * - El par puede contener combinaciones de "🗿" (Rock), "📄" (Paper),
+# *   "✂️" (Scissors), "🦎" (Lizard) o "🖖" (Spock).
 # * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
 # * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
 
@@ -16,7 +16,7 @@
 
 import random
 
-who_wins = {
+WHO_WINS = {
     '🗿': ['🦎', '✂️'],
     '📄': ['🗿', '🖖'],
     '✂️': ['📄', '🦎'],
@@ -24,7 +24,7 @@ who_wins = {
     '🖖': ['🗿', '✂️'],
 }
 
-moves = {
+MOVES = {
     '1':'🗿',
     '2':'📄',
     '3':'✂️',
@@ -32,35 +32,48 @@ moves = {
     '5':'🖖'
 }
 
+def check_input(expression_input):
+    while True:
+        number_characters = input(expression_input)
+        try:
+            if int(number_characters) > 0 and int(number_characters) < 6:
+                print(f'>>> Your move is:  {int(number_characters)}')
+                return number_characters
+            else:
+                print('>>> ERROR! Only accept numbers between 1 and 5.\n')
+        except:
+            print('>>> ERROR! Only accept numbers between 1 and 5.\n')
+
 
 def main():
-    print('Bienvenido al juego "Piedra, Papel, Tijeras, Lagarto, Spock".\n')
-    print('Menu de movimientos:\n1- 🗿 Piedra\n2- 📄 Papel\n3- ✂️  Tijeras\n4- 🦎 Lagarto\n5- 🖖 Spock\n')
+    print('>>> Welcome to the game: "Rock, Paper, Scissors, Lizard, Spock".\n')
+    print('>>> Win the best of 5 games.\n')
+    print('>>> Movement menu:\n1- 🗿 Rock\n2- 📄 Paper\n3- ✂️  Scissors\n4- 🦎 Lizard\n5- 🖖 Spock\n')
     
     player = 0
     cpu = 0
     cont = 0
 
     while cont < 5:
-        player_move = input('Player 1 - Inserta el número de tu movimiento. ')
+        player_move = check_input('>>> Player insert the number of your move. ')
         cpu_move = str(random.randint(1,5))
 
         if player_move != cpu_move:
-            if moves[cpu_move] in who_wins[moves[player_move]]:
+            if MOVES[cpu_move] in WHO_WINS[MOVES[player_move]]:
                 player += 1
             else:
                 cpu += 1
         
-        print(f'{moves[player_move]} vs {moves[cpu_move]}')
+        print(f'>>> {MOVES[player_move]} vs {MOVES[cpu_move]}')
         cont +=1
 
     
     if player != cpu:
         if player > cpu:
-            print('The Winer is Player')
+            print('>>> The Winer is Player')
         else:
-            print('The Winer is CPU')
+            print('>>> The Winer is CPU')
     else:
-        print('Tie!!')
+        print('>>> Tie!!')
 
 main()
